5976bf183410ac62a6fea0c8cb8197cae85d16cc
mastermyka <mastermyka@wp.pl>
2025-11-05 20:28:14 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-window.firebaseConfig = {};
+window.firebaseConfig = {
+  apiKey: "AIzaSyCQEmMVN9pG5pQ47oPFEHzX67kXn29xFXU",
+  authDomain: "obowiazki-b5c14.firebaseapp.com",
+  projectId: "obowiazki-b5c14",
+  storageBucket: "obowiazki-b5c14.firebasestorage.app",
+  messagingSenderId: "1078042843319",
+  appId: "1:1078042843319:web:391cb09c812a6fb7041532"
+};
