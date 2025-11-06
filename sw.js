2a3f8b938e322ce32e9e28b302a4d3a036c77f1e
mastermyka <mastermyka@wp.pl>
2025-11-06 22:29:58 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
+  console.log('[SW] Activated v11');
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
