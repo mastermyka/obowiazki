995c000d763845a53cef13365aa0d044ad03d97f
mastermyka <mastermyka@wp.pl>
2025-11-07 00:30:33 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,5 +4,5 @@ window.firebaseConfig = {
   projectId: "obowiazki-b5c14",
   storageBucket: "obowiazki-b5c14.firebasestorage.app",
   messagingSenderId: "1078042843319",
-  appId: "1:1078042843319:web:2b8d29ee41fa53c7041532"   // ← TEN z konsoli
+  appId: "1:1078042843319:web:2b8d29ee41fa53c7041532"
 };
