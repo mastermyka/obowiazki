0ac707e856e5d25b8bed3ba8db8491b5b29d0ca5
mastermyka <mastermyka@wp.pl>
2025-11-07 00:27:29 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyCQEmMVN9pG5pQ47oPFEHzX67kXn29xFXU",
   authDomain: "obowiazki-b5c14.firebaseapp.com",
   projectId: "obowiazki-b5c14",
   storageBucket: "obowiazki-b5c14.firebasestorage.app",
   messagingSenderId: "1078042843319",
-  appId: "1:1078042843319:web:2b8d29ee41fa53c7041532"
+  appId: "1:1078042843319:web:2b8d29ee41fa53c7041532"   // ← TEN z konsoli
 };
